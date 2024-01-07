033ce69e7f1dcabf11fed40033ffd2a8971b865a
avcoder <avcoder@users.noreply.github.com>
2024-01-06 21:32:02 -0500

    destructure useContext return value

--- src/components/EmailBody.tsx
+++ src/components/EmailBody.tsx
@@ -5,16 +5,16 @@ const EmailBody: React.FC = () => {
   // OR function EmailBody({ title, name }: EmailBody) {
   const randomDate: number = new Date() as unknown as number;
   const date = new Date(randomDate - Math.random() * 1e9).toString();
-  const emailInfo = useContext(EmailContext) as EmailData;
+  const { subject, name, body } = useContext(EmailContext) as EmailData;
 
   return (
     <>
       <div className="email-content">
         <div className="email-content-header pure-g">
           <div className="pure-u-1-2">
-            <h1 className="email-content-title">{emailInfo.subject}</h1>
+            <h1 className="email-content-title">{subject}</h1>
             <p className="email-content-subtitle">
-              From <a>{emailInfo.name}</a> at <span>{date}</span>
+              From <a>{name}</a> at <span>{date}</span>
             </p>
           </div>
 
@@ -28,11 +28,11 @@ const EmailBody: React.FC = () => {
         </div>
 
         <div className="email-content-body">
-          {emailInfo.body}
+          {body}
           <p>
             Regards,
             <br />
-            {emailInfo.name}
+            {name}
           </p>
         </div>
       </div>
